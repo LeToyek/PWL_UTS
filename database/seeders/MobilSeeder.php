6640e9ccba93fe99784506816f789047bfe1a24c
lailatulbadriyah24 <lailatulbadriyah654@gmail.com>
2023-04-11 15:45:13 +0700

    update data seeder

--- database/seeders/MobilSeeder.php
+++ database/seeders/MobilSeeder.php
@@ -16,50 +16,91 @@ class MobilSeeder extends Seeder
     public function run()
     {
         //
+        $mobil = array(
+            array(
+                'merk' => 'Toyota',
+                'tipe' => 'Avanza',
+                'warna' => 'Putih',
+                'jml_penumpang' => 7,
+                'harga' => 20000000,
+                'qty' => 3
+            ),
+            array(
+                'merk' => 'Honda',
+                'tipe' => 'Jazz',
+                'warna' => 'Hitam',
+                'jml_penumpang' => 5,
+                'harga' => 25000000,
+                'qty' => 2
+            ),
+            array(
+                'merk' => 'Mitsubishi',
+                'tipe' => 'Pajero Sport',
+                'warna' => 'Merah',
+                'jml_penumpang' => 7,
+                'harga' => 45000000,
+                'qty' => 1
+            ),
+            array(
+                'merk' => 'Suzuki',
+                'tipe' => 'Ertiga',
+                'warna' => 'Silver',
+                'jml_penumpang' => 7,
+                'harga' => 18000000,
+                'qty' => 4
+            ),
+            array(
+                'merk' => 'Daihatsu',
+                'tipe' => 'Terios',
+                'warna' => 'Putih',
+                'jml_penumpang' => 7,
+                'harga' => 22000000,
+                'qty' => 2
+            ),
+            array(
+                'merk' => 'Nissan',
+                'tipe' => 'X-Trail',
+                'warna' => 'Abu-abu',
+                'jml_penumpang' => 7,
+                'harga' => 38000000,
+                'qty' => 1
+            ),
+            array(
+                'merk' => 'Isuzu',
+                'tipe' => 'Elf',
+                'warna' => 'Hijau',
+                'jml_penumpang' => 17,
+                'harga' => 50000000,
+                'qty' => 1
+            ),
+            array(
+                'merk' => 'Mercedes-Benz',
+                'tipe' => 'E-Class',
+                'warna' => 'Hitam',
+                'jml_penumpang' => 5,
+                'harga' => 150000000,
+                'qty' => 1
+            ),
+            array(
+                'merk' => 'Tesla',
+                'tipe' => 'Model S',
+                'warna' => 'Putih',
+                'jml_penumpang' => 5,
+                'harga' => 250000000,
+                'qty' => 1
+            ),
+            array(
+                'merk' => 'Ferrari',
+                'tipe' => 'F8 Tributo',
+                'warna' => 'Merah',
+                'jml_penumpang' => 2,
+                'harga' => 500000000,
+                'qty' => 1
+            )
+        );
+        
         DB::table('mobil')->insert(
-            [
-                [
-                    "merk" => "Honda Civic RS",
-                    "tipe" => "Sedan",
-                    "warna" => "Merah",
-                    "jml_penumpang" => 4,  
-                    "harga" => 601000000,     
-                    "qty" => 10          
-                ],
-                [
-                    "merk" => "Toyota Corolla Altis Hybrid",
-                    "tipe" => "Sedan",
-                    "warna" => "Hitam",
-                    "jml_penumpang" => 4,  
-                    "harga" => 594000000,     
-                    "qty" => 15  
-                ],
-                [
-                    "merk" => "Jeep Compass Limited",
-                    "tipe" => "Jeep",
-                    "warna" => "Putih",
-                    "jml_penumpang" => 6,  
-                    "harga" => 826000000,     
-                    "qty" => 10 
-                ],
-                [
-                    "merk" => "Hyundai Grand i10",
-                    "tipe" => "Hyundai",
-                    "warna" => "Putih",
-                    "jml_penumpang" => 6,  
-                    "harga" => 184000000,     
-                    "qty" => 5 
-                ]
-                ,
-                [
-                    "merk" => "Jeep Gladiator Rubicon",
-                    "tipe" => "Jeep",
-                    "warna" => "Hitam",
-                    "jml_penumpang" => 4,  
-                    "harga" => 2000000000,     
-                    "qty" => 10 
-                ]
-            ]
+           $mobil
         );
     }
 }
