e1d1154ddde9565733c2b6708b198444e49ca91a
lailatulbadriyah24 <lailatulbadriyah654@gmail.com>
2023-04-12 09:19:27 +0700

    [UPDATE] Mengedit Isi Menu Home

--- resources/views/home.blade.php
+++ resources/views/home.blade.php
@@ -1,23 +1,25 @@
 @extends('layouts.template',["title" => "home"])
 
 @section('content')
-<div class="container">
-    <div class="row justify-content-center">
-        <div class="col-md-8">
-            <div class="card">
-                <div class="card-header">{{ __('Dashboard') }}</div>
+<div class="content-wrapper pt-4">
+    <div class="" style="height: 100%;align">
 
-                <div class="card-body">
-                    @if (session('status'))
-                        <div class="alert alert-success" role="alert">
-                            {{ session('status') }}
-                        </div>
-                    @endif
-
-                    {{ __('You are logged in!') }}
+            <div class="col-md-12">
+                <div class="card">
+                    <div class="card-header">{{ __('Dashboard') }}</div>
+    
+                    <div class="card-body">
+                        @if (session('status'))
+                            <div class="alert alert-success" role="alert">
+                                {{ session('status') }}
+                            </div>
+                        @endif
+    
+                        {{ __('You are logged in!') }}
+                    </div>
                 </div>
             </div>
-        </div>
+
     </div>
 </div>
 @endsection
